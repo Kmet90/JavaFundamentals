1378901079666e848cbf8be6ccc4969e5cbadfef
Kmet90 <kmet90@gmail.com>
2020-11-20 21:51:34 +0000

    Java Fundamentals

--- src/BasicSyntax_ConditionalStatementsAndLoopsLab/PassedOrFailed03.java
+++ src/BasicSyntax_ConditionalStatementsAndLoopsLab/PassedOrFailed03.java
@@ -5,6 +5,12 @@ import java.util.Scanner;
 public class PassedOrFailed03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        double score = Double.parseDouble(scanner.nextLine());
 
+        if (score >= 3) {
+            System.out.println("Passed!");
+        }else {
+            System.out.println("Failed!");
+        }
     }
 }
