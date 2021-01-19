4a1b5322956876a08daf15832d9efad09fb819a4
Kmet90 <kmet90@gmail.com>
2021-01-19 19:49:27 +0000

    Java Fundamentals

---
+++ src/DataTypesAndVariablesLab/LowerOrUpper08.java
@@ -0,0 +1,10 @@
+package DataTypesAndVariablesLab;
+
+import java.util.Scanner;
+
+public class LowerOrUpper08 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
--- src/DataTypesAndVariablesLab/ReversedChars07.java
+++ src/DataTypesAndVariablesLab/ReversedChars07.java
@@ -2,15 +2,14 @@ package DataTypesAndVariablesLab;
 
 import java.util.Scanner;
 
-public class CharsToString06 {
+public class ReversedChars07 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         char ch1 = scanner.nextLine().charAt(0);
         char ch2 = scanner.nextLine().charAt(0);
         char ch3 = scanner.nextLine().charAt(0);
-        String all = ch1 + Character.toString(ch2) + ch3;
 
-        System.out.println(all);
+        System.out.printf("%c %c %c", ch3, ch2, ch1);
     }
 }
