377f93ff9e42528de9f56db1e368d0fbbf98086e
Kmet90 <kmet90@gmail.com>
2021-01-21 23:31:29 +0000

    Java Fundamentals

---
+++ src/DataTypesAndVariablesLab/RefactorVolumeOfPyramid11.java
@@ -0,0 +1,20 @@
+package DataTypesAndVariablesLab;
+
+import java.util.Scanner;
+
+public class RefactorVolumeOfPyramid11 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double length, width, height;
+        System.out.print("Length: ");
+        length = Double.parseDouble(scanner.nextLine());
+        System.out.print("Width: ");
+        width = Double.parseDouble(scanner.nextLine());
+        System.out.print("Height: ");
+        height = Double.parseDouble(scanner.nextLine());
+
+        double volume = (length * height * width) / 3;
+
+        System.out.printf("Pyramid Volume: %.2f", volume);
+    }
+}
