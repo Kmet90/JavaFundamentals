a923cf3a469c33f992bfa686a7c109992b82dbe1
Kmet90 <kmet90@gmail.com>
2021-02-05 18:42:07 +0000

    Java Fundamentals

---
+++ src/MethodsLab/Grades_02.java
@@ -0,0 +1,26 @@
+package MethodsLab;
+
+import java.util.Scanner;
+
+public class Grades_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double score = Double.parseDouble(scanner.nextLine());
+
+        receivesGrade(score);
+    }
+
+    private static void receivesGrade(double grade) {
+        if (grade >= 2 && grade < 3) {
+            System.out.println("Fail");
+        } else if (grade < 3.50) {
+            System.out.println("Poor");
+        } else if (grade < 4.50) {
+            System.out.println("Good");
+        } else if (grade < 5.50) {
+            System.out.println("Very good");
+        } else if (grade <= 6.00) {
+            System.out.println("Excellent");
+        }
+    }
+}
