54d5fbefc4b648bead6b10932c138bb3a5f21c70
Kmet90 <kmet90@gmail.com>
2021-03-08 07:30:17 +0000

    Java Fundamentals

---
+++ src/ProgrammingFundamentalsMidExam/Problem3.java
@@ -0,0 +1,57 @@
+package ProgrammingFundamentalsMidExam;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Problem3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<String> books = Arrays.stream(scanner.nextLine().split("&"))
+                .collect(Collectors.toList());
+        String command = scanner.nextLine();
+
+        while (!command.equals("Done")) {
+            String[] tokens = command.split(" \\| ");
+            String action = tokens[0];
+            switch (action) {
+                case "Add Book":
+                    String addBook = tokens[1];
+                    if (!books.contains(addBook)) {
+                        books.add(0, addBook);
+                    }
+                    break;
+                case "Take Book":
+                    String removeBook = tokens[1];
+                    books.remove(removeBook);
+                    break;
+                case "Swap Books":
+                    String book1 = tokens[1];
+                    String book2 = tokens[2];
+                    if (books.contains(book1) && books.contains(book2)) {
+                        int index1 = books.indexOf(book1);
+                        int index2 = books.indexOf(book2);
+                        Collections.swap(books, index1, index2);
+                    }
+                    break;
+                case "Insert Book":
+                    String insertBook = tokens[1];
+                    if (!books.contains(insertBook)) {
+                        books.add(insertBook);
+                    }
+                    break;
+                case "Check Book":
+                    int index = Integer.parseInt(tokens[1]);
+                    if (index < 0 || index > books.size()) {
+                        break;
+                    } else {
+                        System.out.println(books.get(index));
+                    }
+                    break;
+            }
+            command = scanner.nextLine();
+        }
+        System.out.println(books.toString().replaceAll("[\\[\\]]", ""));
+
+    }
+}
